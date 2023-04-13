016713c8f386e142a8022aeed54091690590afae
suryaa6666 <suryaelidanto@gmail.com>
2023-04-14 01:21:01 +0700

    feat: add gambarin CLI tool
    This commit adds a new CLI tool called gambarin. It allows users to generate images using OpenAI's DALL-E API. The tool has two commands: `--api_key` to set the API key and `--p` to generate an image. The API key is saved in a file called `api_key.txt`. The tool uses the `axios` and `fs` packages.

---
+++ bin/index.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+const axios = require('axios')
+const fs = require('fs')
+
+const getCommand = process.argv.slice(2)
+
+let api_key = ""
+let p = false
+
+getCommand.forEach((item) => {
+    const check = item.split('=')
+    if (check.includes("api_key")) {
+        api_key = check[1]
+    } else if (check.includes("p")) {
+        p = true
+    }
+})
+
+if (api_key) {
+    fs.writeFileSync('api_key.txt', api_key)
+    console.log('API key saved successfully!');
+} else if (p) {
+    async function generateImage() {
+        if (!fs.existsSync('api_key.txt')) {
+            return console.log('Please set your API key first!')
+        }
+        const get_api_key = fs.readFileSync('api_key.txt', 'utf-8')
+        try {
+            const body = {
+                prompt: process.argv.slice(2)[0],
+                n: 1,
+                size: "1024x1024"
+            }
+            const response = await axios.post(`https://api.openai.com/v1/images/generations`, body, {
+                headers: {
+                    Authorization: `Bearer ${get_api_key}`
+                },
+            })
+            console.log("✍️  Created By : SuryaElz")
+            console.log("😇 Follow Me  : https://github.com/suryaa6666")
+            console.log("🙌 Your image : ", response.data.data[0].url)
+        } catch (err) {
+            console.log(`There is an when generating image : ${err.message}`)
+        }
+    }
+
+    generateImage()
+} else {
+    console.log('Please provide a correct command!')
+}
+
